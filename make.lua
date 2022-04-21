8e8a8187477c1b3f80f72457406b71e88043cd1b
actboy168 <actboy168@gmail.com>
2022-04-21 11:45:44 +0800

    整理代码

--- make.lua
+++ make.lua
@@ -20,53 +20,10 @@ if not pcall(require, "env") then
     lm.BimgDir = lm:path "./bimg/"
 end
 
-lm.cxx = "c++17"
-
-lm.warnings = {
-    "error",
-    "on"
-}
-
-lm.defines = "BX_CONFIG_DEBUG=" .. (lm.mode == "debug" and 1 or 0)
-
-lm.msvc = {
-    defines = "_CRT_SECURE_NO_WARNINGS",
-    includes = lm.BxDir / "include/compat/msvc",
-}
-
-lm.mingw = {
-    includes = lm.BxDir / "include/compat/mingw",
-}
-
-lm.macos = {
-    includes = lm.BxDir / "include/compat/osx",
-}
-
-lm.ios = {
-    includes = lm.BxDir / "include/compat/ios",
-    flags = {
-        "-fembed-bitcode",
-        "-Wno-unused-function"
-    }
-}
-
-lm.clang = {
-    flags = {
-        "-Wno-unknown-warning-option",
-        "-Wno-tautological-constant-compare",
-        "-Wno-unused-variable",
-        "-Wno-unused-but-set-variable"
-    }
-}
-
+require "common"
 require "core.bx"
 require "core.bimg"
 require "core.bgfx"
-
-if lm.os == "ios" then
-    return
-end
-
 require "examples.common"
 
 lm:runlua "generator" {
